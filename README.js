137df2ed8afa999f4c1317c8548069e061727197
QmodsX <new1234604@gmail.com>
2014-12-08 14:39:12 +0700

    Create README.js

---
+++ README.js
@@ -0,0 +1,6 @@
+ModPE-Project2
+==============
+var QmodsX = newPar();
+QmodsX.awesome= true Lol
+first one will still be continued but this just for backup
+ill try to  github more often  write code ;)
